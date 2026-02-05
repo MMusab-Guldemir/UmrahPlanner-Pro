da954cd522fa32634b480cb10b8f8c11b1755e12
Muhammed <mmusabgldmr@gmail.com>
2026-02-05 12:20:00 +0300

    .
    .

--- src/model/Hotel.java
+++ src/model/Hotel.java
@@ -1,5 +1,17 @@
 package src.model;
 
 public class Hotel {
-    
+
+    private String hotelId;
+    private String hotelName;
+    private String city;
+    private int starRating;
+    private String roomType;
+    private String distanceToHaram;
+    private double pricePerNight;
+    private int numberOfNights;
+    private double rating;
+    private String[] amenities; // Olanaklar [örneğin: WiFi, kahvaltı, havuz] vb.
+    private boolean haramView;
+
 }
