f07178efd94f2e5039bde4376de0cc55d52d1895
Muhammed <mmusabgldmr@gmail.com>
2026-02-06 10:14:35 +0300

    .
    .

--- src/model/Booking.java
+++ src/model/Booking.java
@@ -1,5 +1,32 @@
 package src.model;
 
+import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Booking {
     
+    private String bookingId;
+    private User user;
+    private Flight outboundFlight;
+    private Flight returnFlight;
+    private Hotel makkahHotel;
+    private Hotel medinaHotel;
+    private List<VisitPlace> visitPlaces;
+    private int numberOfTravelers;
+    private LocalDate travelDate;
+    private boolean hasGuide;
+    private double guideFee;
+    private boolean hasVisa;
+    private double visaFee;
+    private boolean hasInsurance;
+    private double insuranceFee;
+    private double totalPrice;
+    private String status;
+    private LocalDateTime createdAt;
+    private String notes;
+
+    
+
 }
