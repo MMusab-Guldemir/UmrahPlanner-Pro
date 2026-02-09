ec1791cc53991c88e2ec236e9faeafeaa4b0da54
Muhammed <mmusabgldmr@gmail.com>
2026-02-09 21:33:53 +0300

    .
    .

--- src/dao/HotelDAO.java
+++ src/dao/HotelDAO.java
@@ -6,5 +6,24 @@ import src.model.Hotel;
 
 public class HotelDAO {
     List<Hotel> hotels;
-     
+    
+    public HotelDAO() {
+        hotels = new ArrayList<>();
+    }
+    
+    public List<Hotel> getAll() {
+        return new ArrayList<>(hotels);
+    }
+
+    public Hotel getById(String id) {
+        if (id == null || id.trim().isEmpty()) {
+            throw new IllegalArgumentException()
+        }
+        for (Hotel hotel : hotels) {
+            if (hotel.getHotelId().equals(id)) {
+                return hotel;
+            }
+        }
+        return null;
+    }
 }
