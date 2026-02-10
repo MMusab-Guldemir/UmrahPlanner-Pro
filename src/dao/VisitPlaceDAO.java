08f295e6befade0618bd0ca728511f6eb9d74146
Muhammed <mmusabgldmr@gmail.com>
2026-02-10 20:05:41 +0300

    .
    .

--- src/dao/VisitPlaceDAO.java
+++ src/dao/VisitPlaceDAO.java
@@ -1,5 +1,18 @@
 package src.dao;
 
+import java.util.List;
+import java.util.ArrayList;
+import src.model.VisitPlace;;
 public class VisitPlaceDAO {
-                
+    
+    private List<VisitPlace> field;
+
+    public VisitPlaceDAO() {
+        this.field = new ArrayList<>();
+    }
+
+    public void getAll() {
+        
+    }
+    
 }
