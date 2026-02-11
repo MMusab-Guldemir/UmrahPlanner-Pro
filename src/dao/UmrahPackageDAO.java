ab7978a7b4c5f5ce3357f07bff792da369a27f8b
Muhammed <mmusabgldmr@gmail.com>
2026-02-11 20:28:28 +0300

    .
    .

--- src/dao/UmrahPackageDAO.java
+++ src/dao/UmrahPackageDAO.java
@@ -1,7 +1,63 @@
 package src.dao;
 
+import src.model.UmrahPackage;
+import java.util.List;
+import java.util.ArrayList;
 public class UmrahPackageDAO {
     
-    private 
+    private List<UmrahPackage> UmrahPackages;
 
+    public UmrahPackageDAO() {
+        this.UmrahPackages = new ArrayList<>();
+    }
+
+    public List<UmrahPackage> getAll() {
+        return new ArrayList<>(UmrahPackages);
+    }
+
+    public UmrahPackage getById(String id) {
+        if (id == null || id.trim().isEmpty()) {
+            throw new IllegalArgumentException("");
+        }
+
+        for (UmrahPackage umrahPackage : UmrahPackages) {
+            if (umrahPackage.getPackageId().equals(id)) {
+                return umrahPackage;
+            }
+        }
+        return null;
+    }
+
+    public void save(UmrahPackage pkg) {
+        if (pkg == null) {
+            throw new IllegalArgumentException("");
+        }
+        UmrahPackages.add(pkg);
+    }
+
+    public void update(UmrahPackage pkg) {
+        if (pkg == null) {
+            throw new IllegalArgumentException("");
+        }
+
+        for (UmrahPackage umrahPackage : UmrahPackages) {
+            if (umrahPackage.getPackageId().equals(pkg.getPackageId())) {
+                UmrahPackages.remove(pkg);
+            }
+        }
+        save(pkg);
+    }
+
+    public void delete(String id) {
+        if (id == null || id.trim().isEmpty()) {
+            throw new IllegalArgumentException("");
+        }
+
+        for (UmrahPackage umrahPackage : UmrahPackages) {
+            if (umrahPackage.getPackageId().equals(id)) {
+                UmrahPackages.remove(UmrahPackages);
+            }
+        }        
+    }
 }
+ 
