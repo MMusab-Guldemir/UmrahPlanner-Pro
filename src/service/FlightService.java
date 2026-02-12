ddedf4c05a89c332f6e3952824bea85dd5440ea6
Muhammed <mmusabgldmr@gmail.com>
2026-02-12 20:58:00 +0300

    .
    .

--- src/service/FlightService.java
+++ src/service/FlightService.java
@@ -1,11 +1,45 @@
 package src.service;
 import src.model.Flight;
 import java.util.List;
+
 import src.dao.FlightDAO;;
 public class FlightService {
     private FlightDAO flightDAO;
 
     public FlightService() {
         this.flightDAO = new FlightDAO();
+    }
+
+    public void addFlight(Flight flight) {
+        if (flight == null) {
+            throw new IllegalArgumentException();
+        }
+        flightDAO.save(flight);
+    }
+
+    public void updateFlight(Flight flight) {
+        if (flight == null) {
+            throw new IllegalArgumentException();
+        }
+        if (flightDAO.getById(flight.getFlightId()) == null) {
+            throw new IllegalArgumentException();
+        }
+        flightDAO.update(flight);
+    } 
+
+    public void deleteFlight(String id ){
+        if (id == null || id.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        Flight existing = flightDAO.getById(id);
+        if (existing == null) {
+            throw new IllegalArgumentException();
+        }
+
+        flightDAO.delete(id);
+    }
+
+    public List<Flight> getAllFlights() {
+        return flightDAO.getAll();
     }
 }
