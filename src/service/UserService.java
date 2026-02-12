0e83a7e2766abf6bf640a3dae66904fee1daad15
Muhammed <mmusabgldmr@gmail.com>
2026-02-12 06:46:59 +0300

    .
    .

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,5 +1,14 @@
 package src.service;
+import src.model.User;
+import src.dao.UserDAO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserService {
-    
+    private UserDAO userDAO;
+
+    public UserService() {
+        this.userDAO = new UserDAO();
+    }
 }
