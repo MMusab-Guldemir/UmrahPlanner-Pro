b6e0fd1a44a0177c6c9a923107ab590ff86de0b9
Muhammed <mmusabgldmr@gmail.com>
2026-02-12 06:59:04 +0300

    .
    .

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -2,8 +2,10 @@ package src.service;
 import src.model.User;
 import src.dao.UserDAO;
 
+import java.lang.classfile.attribute.PermittedSubclassesAttribute;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class UserService {
     private UserDAO userDAO;
@@ -11,4 +13,21 @@ public class UserService {
     public UserService() {
         this.userDAO = new UserDAO();
     }
+
+    public void registerUser(User user) {
+        if (user == null) {
+            throw new IllegalArgumentException();
+        }
+
+        User existing = userDAO.getByTcNumber(user.getTcNumber());
+
+        if (existing != null) {
+            throw new IllegalArgumentException("");
+        }
+
+        userDAO.save(user);
+    }
+
+    
+
 }
