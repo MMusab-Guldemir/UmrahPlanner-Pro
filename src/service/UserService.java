7d9a058520ffc6f700d9b3db9726f76a6c889b17
Muhammed <mmusabgldmr@gmail.com>
2026-02-12 07:10:02 +0300

    .
    .

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,11 +1,9 @@
 package src.service;
 import src.model.User;
 import src.dao.UserDAO;
-
-import java.lang.classfile.attribute.PermittedSubclassesAttribute;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadPoolExecutor;
+
 
 public class UserService {
     private UserDAO userDAO;
@@ -28,6 +26,57 @@ public class UserService {
         userDAO.save(user);
     }
 
-    
+    public User updateUser(User user) {
+        if (user == null) {
+            throw new IllegalArgumentException("");
+        }
+        
+        User existing = userDAO.getById(user.getUserId());
+        
+        if (existing == null) {
+            throw new IllegalArgumentException("");
+        }
+        userDAO.update(user);
+        return existing;        
+    }
+
+    public User deleteUser(String id) {
+        if (id == null || id.trim().isEmpty()) {
+            throw new IllegalArgumentException(""); 
+        }
+
+        User existing = userDAO.getById(id);
+
+        if (existing == null) {
+            throw new IllegalArgumentException("");
+        }
+
+        userDAO.delete(id);
+        return existing;
+    }
+
+    public User getUserById(String id) {
+        User existing = userDAO.getById(id);
+        if (existing == null) {
+            throw new IllegalArgumentException("");
+        }
+        return existing;
+    }
+
+    public List<User> getAllUsers() {
+        return userDAO.getAll();
+    }
+
+    public User findByTcNumber(String tc) {
+        return userDAO.getByTcNumber(tc);
+    }
+
+    public List<User> findByFullName(String name) {
+        return userDAO.getByFullName(name);
+    }
+
+    public List<User> findBBByNationality(String nationality) {
+        return userDAO.getByNationality(nationality);
+    }
 
 }
