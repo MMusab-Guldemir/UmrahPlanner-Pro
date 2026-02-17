0b90f662199b63b9fa47dee37fa3ce6781ade663
Muhammed <mmusabgldmr@gmail.com>
2026-02-17 10:57:37 +0300

    .
    .

--- src/service/BookingService.java
+++ src/service/BookingService.java
@@ -1,5 +1,17 @@
 package src.service;
+import src.dao.BookingDAO;
 
 public class BookingService {
-    
+    private BookingDAO bookingDAO;
+    private UserService userService;
+    private HotelService hotelService;
+    private FlightService flightService;
+
+    public BookingService() {
+        this.bookingDAO = new BookingDAO();
+        this.userService = new UserService();
+        this.hotelService = new HotelService();
+        this.flightService = new FlightService();
+    }
+
 }
