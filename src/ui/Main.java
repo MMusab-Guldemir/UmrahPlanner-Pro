7c2d6b5d88643530588219e32d752624a95fe747
Muhammed <mmusabgldmr@gmail.com>
2026-02-17 15:41:39 +0300

    .
    .

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,15 @@
 package src.ui;
+import src.service.BookingService;
+import src.service.FlightService;
+import src.service.HotelService;
+import src.service.UserService;
+import java.util.Scanner;
 
+import src.dao.*;
 public class Main {
-    
+    private UserService userService;
+    private HotelService hotelService;
+    private FlightService flightService;
+    private BookingService bookingService;
+    private Scanner scanner;
 }
