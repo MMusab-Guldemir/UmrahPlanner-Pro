47df7bc1098b004228cf5d2cd981d568ac416436
Muhammed <mmusabgldmr@gmail.com>
2026-02-17 15:53:10 +0300

    .
    .

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,6 +3,8 @@ import src.service.BookingService;
 import src.service.FlightService;
 import src.service.HotelService;
 import src.service.UserService;
+
+import java.lang.Thread.State;
 import java.util.Scanner;
 
 import src.dao.*;
@@ -12,4 +14,82 @@ public class Main {
     private FlightService flightService;
     private BookingService bookingService;
     private Scanner scanner;
+
+    public Main() {
+        this.userService = new UserService();
+        this.hotelService = new HotelService();
+        this.flightService = new FlightService();
+        this.bookingService = new BookingService();
+        this.scanner = new Scanner(System.in);
+
+    }
+
+    public static void main(String[] args) {
+        Main program = new Main();
+    }
+
+    public void run() {
+        while (true) {
+
+        }
+    }
+
+    public void showMainMenu() {
+        System.out.println(" ═══════════════════════════\n" + 
+                        "   UMRAH TRAVEL MANAGEMENT\n" + 
+                        "   ═══════════════════════════\n" + 
+                        "   1. Kullanıcı İşlemleri\n" + 
+                        "   2. Otel İşlemleri\n" + 
+                        "   3. Uçuş İşlemleri\n" + 
+                        "   4. Rezervasyon İşlemleri\n" + 
+                        "   5. Çıkış");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                System.out.println("Güle güle!");
+                break;
+        }
+    }
+
+    public void showUserMenu() {
+        System.out.println("1. Yeni Kullanıcı Ekle\n" + 
+                        "   2. Tüm Kullanıcıları Listele\n" + 
+                        "   3. Kullanıcı Ara (TC ile)\n" + 
+                        "   4. Kullanıcı Sil\n" + 
+                        "   5. Ana Menüye Dön");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                System.out.println("Güle güle!");
+                break;
+        }
+    }
+    
 }
