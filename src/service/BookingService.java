192271a64e64aadf28946dc319a68d82af51c86f
Muhammed <mmusabgldmr@gmail.com>
2026-02-17 11:02:40 +0300

    .
    .

--- src/service/BookingService.java
+++ src/service/BookingService.java
@@ -1,6 +1,8 @@
 package src.service;
 import src.dao.BookingDAO;
-
+import java.util.List;
+import src.model.Booking;
+import java.time.LocalDate;
 public class BookingService {
     private BookingDAO bookingDAO;
     private UserService userService;
@@ -12,6 +14,27 @@ public class BookingService {
         this.userService = new UserService();
         this.hotelService = new HotelService();
         this.flightService = new FlightService();
+    }
+
+    public void createBooking(Booking booking) {
+        if (booking == null)  {
+            throw new IllegalArgumentException();
+        }
+        PriceCalculator.calculateTotalBookingPrice(booking);
+        booking.setTotalPrice(booking.getTotalPrice());
+        bookingDAO.save(booking);
+    }
+
+    public void updateBooking(Booking booking) {
+        if (booking == null) {
+            throw new IllegalArgumentException();
+        }
+        if (bookingDAO.getById(booking.getBookingId()) == null) {
+            throw new IllegalArgumentException();
+        }
+
+        booking.setTotalPrice(booking.getTotalPrice());
+        bookingDAO.update(booking);
     }
 
 }
